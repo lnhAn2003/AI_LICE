c733894021bcbba3feef0105fc3909efae0f4283
lnhAn2003 <lenghoaian0508@gmail.com>
2024-12-11 11:47:24 +0700

    feature: update course and lesson component

--- Back-end/src/routes/course.routes.ts
+++ Back-end/src/routes/course.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import multer from 'multer';
 import courseController from "../controllers/course.controller";
 import { authenticateJWT } from "../middlewares/auth.middleware";
 import { logActivity } from "../middlewares/log.middleware";
 
 const router = Router();
+const upload = multer();
 
 // Create a new course
-router.post("/", authenticateJWT, logActivity("user_created_course"), courseController.createCourse.bind(courseController));
+router.post("/", authenticateJWT, logActivity("user_created_course"), upload.fields([{ name: "images", maxCount: 5 }, { name: "resources", maxCount: 10 }]),courseController.createCourse.bind(courseController));
 
 // Get all courses
 router.get("/",logActivity("user_viewed_courses"), courseController.getAllCourses.bind(courseController));
@@ -15,7 +17,7 @@ router.get("/",logActivity("user_viewed_courses"), courseController.getAllCourse
 router.get("/:id", logActivity("user_viewed_course"), courseController.getCourseById.bind(courseController));
 
 // Update a course
-router.patch("/:id", authenticateJWT, logActivity("user_updated_course"), courseController.updateCourse.bind(courseController));
+router.patch("/:id", authenticateJWT, logActivity("user_updated_course"), upload.fields([{ name: "images", maxCount: 5 }, { name: "resources", maxCount: 10 }]), courseController.updateCourse.bind(courseController));
 
 // Delete a course
 router.delete( "/:id", authenticateJWT, logActivity("user_deleted_course"), courseController.deleteCourse.bind(courseController));
